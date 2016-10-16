c3175b5934c77c545fafa9dbe7ba4847e4fe11e7
Surya Teja <suryaparsa@gmail.com>
2016-10-16 14:44:02 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,15 +5,24 @@ var counter = 0;
 button.onclick = function(){
     
     // Make a request to counter end point
+    var request = new XMLHttpRequest();
     
     //Capture the response and store it in a variable
+    request.onreadystatechange = function(){
+      
+      if(request.status == 200){
+          //take some action
+            var counter = request.responseText();
+            var span = document.getElementById('count');
+            span.innerHTML = counter.toString();      
+      }
+      //do nothing
+        
+    };
     
-    //Render the variable in correct span
-    var span = document.getElementById('count');
-    
-    counter = counter + 1 ;
-    
-    span.innerHTML = counter.toString();
+    //make a request 
+    request.open('GET','http://suryachintu.imad.hasura-app.io/counter',true);
+    request.send(null);
     
     
 };
