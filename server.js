db18a8c232465d617e1756c43b0eccd837ce321c
Surya Teja <suryaparsa@gmail.com>
2016-11-06 15:54:24 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -53,7 +53,7 @@ function createTemplate(data){
 function hash(input){
     
     var hashed = crypto.pbkdf2Sync(input, 'salt', 10000, 5, 'sha512');
-    return hashed.toString('hex');
+    return ["pbkdf2",10000,"salt",hashed.toString('hex')].join('$');
 }
 
 app.get('/hash/:input', function (req, res) {
