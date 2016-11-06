bbd9e15be372c44d5054b6e9bcc94c0f85300ecc
Surya Teja <suryaparsa@gmail.com>
2016-11-06 15:40:24 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
-
+var crypto = require('crypto');
 var config = {
   user: 'suryachintu',
   database: 'suryachintu',
@@ -49,6 +49,19 @@ function createTemplate(data){
         `;
         return htmlTemplate;
 }
+
+function hash(input){
+    
+    var hashed = crypto.pbkdf2Sync(input, 'salt', 10000, 512, 'sha512');
+    return hashed.toString('hex');
+}
+
+app.get('/hash/:input', function (req, res) {
+    
+    var hashedString = hash(req.params.input);
+    res.send(hashedString);
+    
+});
 
 var pool = new Pool(config);
 app.get('/test-db', function (req, res) {
