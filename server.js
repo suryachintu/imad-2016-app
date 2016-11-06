38916fbaccf710a96ce44adc9211a7eec971a578
Surya Teja <suryaparsa@gmail.com>
2016-11-06 15:03:56 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -107,9 +107,21 @@ app.get('/submit-name', function (req, res) {
 });
 
 //my urls
-app.get('/:articleName',function(req,res){
-    var articleName=req.params.articleName;
-    res.send(createTemplate(articles[articleName]));
+app.get('/articles/:articleName',function(req,res){
+    
+    pool.query("SELECT * FROM article WHERE title = " + req.params.articleName ,function(err,result){
+        if(err){
+            res.status(500).send(err.toString());
+        }else{
+            if(result.rows === 0){
+                res.status(404).send("Article Not Found");
+            }else{
+                var articleData = result.rows[0];
+                res.send(createTemplate(articleData));
+            }
+        }
+    });
+    
 }
 );
 app.get('/ui/style.css', function (req, res) {
