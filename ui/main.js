54ac591224a305a92ecc15b6a13df1a789a22536
Surya Teja <suryaparsa@gmail.com>
2016-11-06 17:49:17 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,34 +1,5 @@
-//counter code
-var button = document.getElementById('counter');
-button.onclick = function(){
-    
-    // Make a request to counter end point
-    var request = new XMLHttpRequest();
-    
-    //Capture the response and store it in a variable
-    request.onreadystatechange = function(){
-      
-      if(request.readyState === XMLHttpRequest.DONE){
-          //Take action
-          if(request.status === 200){
-              //take some action
-                var counter = request.responseText;
-                var span = document.getElementById('count');
-                span.innerHTML = counter.toString();      
-          }
-      }
-        
-    };
-    
-    //make a request 
-    request.open('GET','http://suryachintu.imad.hasura-app.io/counter',true);
-    request.send(null);
-    
-    
-};
 
-//Submit name
-var nameInput = document.getElementById('name');
+//Submit username password
 var submit = document.getElementById('submit_btn');
 submit.onclick = function(){
     //Make a request to the server and send name
@@ -41,26 +12,21 @@ submit.onclick = function(){
       if(request.readyState === XMLHttpRequest.DONE){
           //Take action
           if(request.status === 200){
-              //take some action
-            //capture a list of names and renser it as names
-            var names = request.responseText;
-            names = JSON.parse(names);
-            var list = '';
-            
-            for(var i=0 ; i<names.length;i++){
-                list+= '<li>' + names[i] + '</li>';  
-            }
-            
-            var ul = document.getElementById('namelist');
-            ul.innerHTML = list;
-             
+            console.log("user logged in");
+            alert("Successfully logged in ");
+          }
+          else (request.status === 403)
+          {
+            alert("invalid credentials");
           }
       }
         
     };
-    var name = nameInput.value;
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+    console.log(username + password);
     //make a request 
-    request.open('GET','http://suryachintu.imad.hasura-app.io/submit-name?name='+name,true);
-    request.send(null);
+    request.open('POST','http://suryachintu.imad.hasura-app.io/login',true);
+    request.send(JSON.stringify({"username":username,"password":password}));
     
 };
