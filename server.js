68b6018a978cf652ac8740435ad3477d22f0dc6a
Surya Teja <suryaparsa@gmail.com>
2016-11-06 16:29:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -50,16 +50,31 @@ function createTemplate(data){
         return htmlTemplate;
 }
 
-function hash(input){
+function hash(input,salt){
     
-    var hashed = crypto.pbkdf2Sync(input, 'salt', 10000, 512, 'sha512');
-    return ["pbkdf2",10000,"salt",hashed.toString('hex')].join('$');
+    var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
+    return ["pbkdf2",10000,salt,hashed.toString('hex')].join('$');
 }
 
 app.get('/hash/:input', function (req, res) {
     
-    var hashedString = hash(req.params.input);
+    var hashedString = hash(req.params.input,"random");
     res.send(hashedString);
+    
+});
+
+app.get('/create-user', function (req, res) {
+    
+    var salt = crypto.getRandomBytes(128).toString('hex');
+    var dbString = hash(password,salt);
+    
+    pool.query('INSET INTO "user" (username,password) VALUES ($1,$2)',[username,dbString],function(err,result){
+        if(err){
+            res.status(500).send(err.toString());
+        }else{
+            res.send("User successfully created "  + username);
+        }
+    });
     
 });
 
