649705eeb78d27aab9ea40cf6fa6d959532c5a2f
Surya Teja <suryaparsa@gmail.com>
2016-11-12 01:44:43 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -58,6 +58,8 @@ function createTemplate(data){
                 <center>Loading comments...</center>
               </div>
         </div>
+        
+          <script type="text/javascript" src="/ui/article.js"></script>
         </body>    
         </html>
         `;
